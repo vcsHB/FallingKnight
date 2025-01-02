c930a846c80e7fc41508c6539f34d9a44d2e94fa
AhnSeonghun <agkTeamN@sdh.hs.kr>
2025-01-03 00:21:05 +0900

    fix: MapTile Scrolling

--- Assets/01.Scrips/Object/Map/ScrollStructure.cs
+++ Assets/01.Scrips/Object/Map/ScrollStructure.cs
@@ -7,7 +7,7 @@ public class ScrollStructure : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector2(transform.position.x, transform.position.y + MapManager.Instance.fallingSpeed * Time.deltaTime);
+        // transform.position = new Vector2(transform.position.x, transform.position.y + MapManager.Instance.fallingSpeed * Time.deltaTime);
 
         if (transform.position.y > Camera.main.transform.position.y + Camera.main.orthographicSize + offset)
         {
