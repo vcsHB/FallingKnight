d3274c98b574fde9136d948cca031597dad4ed5c
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-04 01:50:51 +0900

    fix: Escaping Map

--- Assets/01.Scrips/Managers/GameManager.cs
+++ Assets/01.Scrips/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Agents.Players;
+using InputSystem;
 using UIManage.InGame;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,12 +13,27 @@ namespace Managers
         [SerializeField] private StoneCollector _stoneCollector;
         [SerializeField] private GameOverPanel _gameOverPanel;
         [SerializeField] private float _waitTerm;
+        [SerializeField] private PlayerInput _playerInput;
+        [SerializeField] private UIInputReader _uiInputReader;
         //[SerializeField] private FadePanel
+
+        private void Awake()
+        {
+            SetInputControl(false);
+            StartCoroutine(GameStartCoroutine());
+        }
+
+        private IEnumerator GameStartCoroutine()
+        {
+            yield return new WaitForSeconds(0.1f);
+            SetInputControl(true);
+        }
 
 
         public void GameOver()
         {
-            
+            Time.timeScale = 0f;
+            SetInputControl(false);
             _gameOverPanel.Open();
         }
 
@@ -30,7 +46,15 @@ namespace Managers
         {
             // 페이드 효과
             yield return new WaitForSeconds(_waitTerm);
+            Time.timeScale = 1f;
+            SetInputControl(true);
             SceneManager.LoadScene("LobbyScene"); // 씬이름 안맞으면 바꾸기
+        }
+
+        public void SetInputControl(bool value)
+        {
+            _playerInput.canControl = value;
+            _uiInputReader.canControl = value;
         }
 
 
