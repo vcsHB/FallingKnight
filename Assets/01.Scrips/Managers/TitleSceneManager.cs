a37464a02e246840dd9dec9d6fb17975a5dd60d0
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-04 04:13:56 +0900

    feat: GameStart Transition

---
+++ Assets/01.Scrips/Managers/TitleSceneManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UIManage;
+
+namespace Managers
+{
+
+    public class TitleSceneManager : MonoBehaviour
+    {
+        [SerializeField] private float _moveToGameSceneTerm = 1f;
+        [Header("Start Animation")]
+        [SerializeField] private UIPanel _fadePanel;
+        [SerializeField] private RectTransform _playerPanelTrm;
+        [SerializeField] private int _jumpNum = 1;
+        [SerializeField] private RectTransform _targetPosTrm;
+        [SerializeField] private float _jumpPower;
+        [SerializeField] private float _duration;
+        public void HandleGameStart()
+        {
+            _playerPanelTrm.DOJumpAnchorPos(_targetPosTrm.anchoredPosition, _jumpPower, _jumpNum, _duration);
+            StartCoroutine(MoveToGameSceneCoroutine());
+        }
+
+        private IEnumerator MoveToGameSceneCoroutine()
+        {
+            _fadePanel.Open();
+            yield return new WaitForSeconds(_moveToGameSceneTerm);
+            SceneManager.LoadScene("VCS_Scene");
+        }
+    }
+
+}
